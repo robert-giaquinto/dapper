85739aa25e4d9da1df7e0dc5ca45fc070ee270d8
robert-giaquinto <giaquinto.ra@gmail.com>
2018-01-04 13:44:57 -0600

    updated state of the union test file, may still need some tweaking though

--- src/experiments/sotu.py
+++ src/experiments/sotu.py
@@ -3,6 +3,7 @@ from src.corpus import Corpus
 from src.dapper import DAPPER
 import time
 import logging
+import os
 
 
 def main():
@@ -15,45 +16,49 @@ def main():
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     np.random.seed(2018)
 
-    sotu = Corpus(input_file="sotu_dpp_dap.txt", vocab_file="sotu_dpp.ldac.vocab")
-    id2author = {y: x for x, y in sotu.author2id.items()}
+    path_to_current_file = os.path.abspath(os.path.dirname(__file__))
+    data_dir = os.path.join(path_to_current_file, "../../data/")
+    sotu = Corpus(input_file=data_dir + "sotu_dpp_dap.txt", vocab_file=data_dir + "sotu_dpp.ldac.vocab")
 
-    num_topics = 15
+    num_topics = 20
     num_personas = 10
 
-    em_iter = 50
+    em_iter = 5
     e_iter = 30
+    em_convergence = 1e-4
     num_workers = 1
 
-    batch_size = 16
+    batch_size = 2048
     learning_offset = 5
     learning_decay = 0.6
     step_size = 0.7
     queue_size = 1
 
-    pn = 0.3
+    pn = 0.4
     mn = 0.8
-    penalty = 0.3
+    regularization = 0.3
     normalization = "sum"
 
     print("=====================STOCHASTIC CVI=====================")
     dap = DAPPER(num_topics=num_topics, num_personas=num_personas,
-              process_noise=pn, measurement_noise=mn, penalty=penalty, normalization=normalization,
-              em_max_iter=em_iter, em_convergence=1e-03,
-              step_size=step_size, local_param_iter=e_iter, batch_size=batch_size, queue_size=queue_size,
-              learning_offset=learning_offset, learning_decay=learning_decay,
-              num_workers=num_workers)
+                 process_noise=pn, measurement_noise=mn,
+                 regularization=regularization, normalization=normalization,
+                 em_max_iter=em_iter, em_convergence=em_convergence,
+                 step_size=step_size, local_param_iter=e_iter,
+                 batch_size=batch_size, queue_size=queue_size,
+                 learning_offset=learning_offset, learning_decay=learning_decay,
+                 num_workers=num_workers)
 
     train_results = dap.fit(corpus=sotu)
     logger.info(dap)
 
-    model_sig = "K{}_P{}_bs{}_lo{}_ld{}_pn{}_mn{}_penalty{}_{}_epochs{}_cpu{}_{}.txt".format(
+    model_sig = "K{}_P{}_bs{}_lo{}_ld{}_pn{}_mn{}_reg{}_{}_epochs{}_cpu{}_{}.txt".format(
         num_topics, num_personas, batch_size, learning_offset, int(100 * learning_decay),
-        int(100*pn), int(100*mn), int(100*penalty), normalization, dap.total_epochs,
+        int(100*pn), int(100*mn), int(100*regularization), normalization, dap.total_epochs,
         num_workers, time.strftime('%m_%d_%Y_%H%M'))
 
-    dap.save_topics(filename="../../results/sotu/topics_" + model_sig, topn=10, tfidf=True)
-    dap.save_author_personas(filename="../../results/sotu/personas_" + model_sig, id2author=id2author)
+    dap.save_topics(filename="../../results/sotu/topics_" + model_sig, topn=10)
+    dap.save_author_personas(filename="../../results/sotu/personas_" + model_sig)
     dap.save_persona_topics(filename="../../results/sotu/alpha_" + model_sig)
     dap.save_convergnces(filename="../../results/sotu/convergence_" + model_sig, results=train_results)
 
